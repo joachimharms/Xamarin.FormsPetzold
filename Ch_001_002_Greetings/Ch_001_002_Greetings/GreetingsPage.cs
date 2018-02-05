3eeb2a4e29f225e045424849babc7d84f300500e
joachimharms <joachimharms77@gmail.com>
2018-02-05 23:42:03 +0100

    Initial commit

---
+++ Ch_001_002_Greetings/Ch_001_002_Greetings/GreetingsPage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Ch_001_002_Greetings
+{
+	public class GreetingsPage : ContentPage
+	{
+		public GreetingsPage ()
+		{
+			Content = new StackLayout {
+				Children = {
+					new Label { Text = "Welcome to Xamarin.Forms!" }
+				}
+			};
+		}
+	}
+}
